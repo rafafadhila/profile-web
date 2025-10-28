96499d0c42df6a1540abd889d8c621c8933d0e9a
rafafadhila <rafafadhila03@gmail.com>
2025-10-28 13:45:57 +0700

    axios-instance url update

--- src/utils/axios-instance.ts
+++ src/utils/axios-instance.ts
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: "http://192.168.18.145:3000/api"
+    baseURL: "http://localhost:3000/api"
 })
+
+// Perlu diganti URL nya pas deploy
+//  localhost
