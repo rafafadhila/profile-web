74aa525e40bf0c10e93379224970144251ad6f77
rafafadhila <rafafadhila03@gmail.com>
2025-11-06 21:12:32 +0700

    update metadata for SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,11 +22,34 @@ const montserrat = Montserrat({
 
 export const metadata: Metadata = {
   title: "Rafa Fadhila Profile Website || Web Developer",
-  description: "",
+  description: "Rafa Fadhila — a web developer focused on crafting responsive, efficient, and visually engaging websites with clean, modern code.",
   keywords: ['Rafa Fadhila Putra', 'Web Developer', 'Web Designer', 'Front End Developer', 'Next.Js', 'Javascript'],
   authors: [{
     name: 'Rafa Fadhila Putra',
-  }]
+  }],
+  openGraph: {
+    title: "Rafa Fadhila Profile Website || Web Developer",
+    description: "Rafa Fadhila — a web developer focused on crafting responsive, efficient, and visually engaging websites with clean, modern code.",
+    url: process.env.NEXT_PUBLIC_BASE_URL,
+    siteName: "Future Fitness",
+    images: [
+      {
+        url: "/images/profile-pic.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Rafa Fadhila Web Profile",
+      },
+    ],
+    locale: "id_ID",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Rafa Fadhila Profile Website || Web Developer",
+    description:
+      "Rafa Fadhila — a web developer focused on crafting responsive, efficient, and visually engaging websites with clean, modern code.",
+    images: ["https://yourwebsite.com/images/profile-pic.jpg"],
+  },
 };
 
 export default function RootLayout({
@@ -40,8 +63,8 @@ export default function RootLayout({
         className={`${montserrat.variable} antialiased`}
       >
         <ThemeProvider>
-        {children}
-        <ToastContainer></ToastContainer>
+          {children}
+          <ToastContainer></ToastContainer>
         </ThemeProvider>
       </body>
     </html>
