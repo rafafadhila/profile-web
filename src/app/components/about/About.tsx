bdd9d6a4465ea30a5019e60eb99d1524a46cb0eb
rafafadhila <rafafadhila03@gmail.com>
2025-11-07 19:10:04 +0700

    fix text allign on mobile

--- src/app/components/about/About.tsx
+++ src/app/components/about/About.tsx
@@ -18,14 +18,14 @@ export default function About() {
       <div className="container max-w-4xl mx-auto px-5 py-10 text-center">
         <h2 className="text-4xl font-semibold mb-12 underline-offset-8 underline decoration-indigo-800">About Me</h2>
 
-        <p className="sm:text-left md:text-justify mb-6 leading-relaxed">
+        <p className="sm:text-center md:text-justify mb-6 leading-relaxed">
           I&apos;m Rafa Fadhila, a Computer Science graduate from Bina Nusantara University.
           I have one year of experience as a web developer intern, where I built and maintained websites using modern web technologies.
         </p>
-        <p className='text-justify mb-6 leading-relaxed'>
+        <p className='sm:text-center md:text-justify mb-6 leading-relaxed'>
           I have experience working with Laravel, JavaScript, PHP, HTML, and CSS to build and maintain websites. I focus on creating functional and clean web applications from both the front-end and back-end sides.
         </p>
-        <p className='text-justify mb-6 leading-relaxed'>
+        <p className='sm:text-center md:text-justify mb-6 leading-relaxed'>
           During my one-year internship as a web developer, I worked as part of a collaborative team where communication and feedback were essential. I learned to work effectively under pressure, manage my time well, and support teammates to achieve shared goals.
         </p>
 
